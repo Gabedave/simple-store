c28a6723930b0547fd6c0a421237dc70284982df
David Gabriel <gabedave01@gmail.com>
2023-12-03 08:10:28 +0100

    chore: add order schema to orders.module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    AuthModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
